5d300e3a6312e546971301fc32a817461f3e6d13
johnnyho <johnnyhohohohohoho@gmail.com>
2023-01-07 16:48:04 +0800

    use synchronized keyword to solve race condition problem

--- counter/src/main/java/com/whizlab/Count.java
+++ counter/src/main/java/com/whizlab/Count.java
@@ -3,7 +3,11 @@ package com.whizlab;
 public class Count {
     private long count = 0;
 
-    public void add() {
+    // use synchonized keyword so that only one thread can access this function at a time
+    // since Increaser1 and Increaser2 access the shared object Count add() method
+    // if the threads access at the same time, the value of count will be updated based on the old value at the same time
+    // this problme is called race condition
+    public synchronized void add() {
         this.count++;
     }
 
